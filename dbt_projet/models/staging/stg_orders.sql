274fcdf6e322bc56c1ba27dc96c3c5a7b75d7fef
simaurice68-dotcom <simaurice68@gmail.com>
2026-06-30 14:14:13 +0200

    ajout du modele staging pour raw_orders

---
+++ dbt_projet/models/staging/stg_orders.sql
@@ -0,0 +1,10 @@
+with source as (
+    select * from {{ source('my_dbt_db', 'raw_orders') }}
+),
+renamed as (
+    select
+        id as order_id,
+        customer as customer_id
+    from source
+)
+select * from renamed
